7877cd874d21d3ebd1dde09e13529d8933abb587
Avinash <avinash.madavan@gmail.com>
2016-06-05 18:51:44 -0700

    Define generic label type (label_t)

---
+++ src/util/ocrtypes.h
@@ -0,0 +1,12 @@
+#ifndef OCR_UTIL_OCRTYPES_H_
+#define OCR_UTIL_OCRTYPES_H_
+
+namespace ocr {
+
+	/**
+	 * Generalized type for labels (32-bit unsigned int)
+	 */
+	typedef uint32_t label_t;
+}
+
+#endif // OCR_UTIL_OCRTYPES_H_
